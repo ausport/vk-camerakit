88c5e1144ec295fbc60b3f3009f380634c15a829
Stuart Morgan <stuart.morgan@ausport.gov.au>
2021-08-25 12:50:25 +1000

    Extended generic camera overrides.

--- cameras/camera_generic.py
+++ cameras/camera_generic.py
@@ -10,11 +10,35 @@ class VKCameraGenericDevice(VKCamera):
 
         self.video_object = cv2.VideoCapture(device)
         if self.video_object.isOpened():
-            self.filepath = "video device {0}".format(device)
             self.device = device
+
+    def eof(self):
+        """Overrides eof.
+
+        Returns:
+            (bool): True if video device is available.
+        """
+        return not self.video_object.isOpened()
 
     def get_frame(self):
         res, frame = self.video_object.read()
         # Pillow assumes RGB - OpenCV reads BRG
         cv2.cvtColor(frame, cv2.COLOR_BGR2RGB, frame)
         return frame
+
+    def __str__(self):
+        """Overriding str
+
+        Returns:
+            (str): A string summary of the object
+        """
+        return "\nCamera Source:" \
+               "\n\tVideo Device     : {0}" \
+               "\n\tWidth            : {1}" \
+               "\n\tHeight           : {2}" \
+               "\n\tFrame Rate       : {3}" \
+               "\n\tFrame Count      : {4}".format(self.device,
+                                                   self.width(),
+                                                   self.height(),
+                                                   self.fps(),
+                                                   self.frame_count())
