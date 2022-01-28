b3b81649fb12d281abec55a44f031f522b1e9c4c
Stuart Morgan <stuart.morgan@ausport.gov.au>
2022-01-28 14:44:33 +1100

    Vimba is not available on osx.

--- cameras/__init__.py
+++ cameras/__init__.py
@@ -2,5 +2,9 @@ from cameras.camera_base import VKCamera
 from cameras.camera_generic import VKCameraGenericDevice
 from cameras.camera_file import VKCameraVideoFile
 from cameras.camera_panoramic import VKCameraPanorama
-from cameras.camera_vimba import VKCameraVimbaDevice
 from cameras.helpers.camera_parser import load_camera_model_from_json
+try:
+    from cameras.camera_vimba import VKCameraVimbaDevice
+except ImportError:
+    print("* Vimba library is not available.")
+
