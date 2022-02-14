fc8593ae0b6986f721ab4d1014164ae1284b5cd7
Stuart Morgan <stuart.morgan@ausport.gov.au>
2022-02-15 09:59:26 +1100

    Refactored module function.

--- cameras/__init__.py
+++ cameras/__init__.py
@@ -2,7 +2,7 @@ from cameras.camera_base import VKCamera
 from cameras.camera_generic import VKCameraGenericDevice
 from cameras.camera_file import VKCameraVideoFile
 from cameras.camera_panoramic import VKCameraPanorama
-from cameras.helpers.camera_parser import load_camera_model_from_json
+from cameras.helpers.camera_parser import load_camera_model_from_json as load_camera_model
 try:
     from cameras.camera_vimba import VKCameraVimbaDevice
 except ImportError:
