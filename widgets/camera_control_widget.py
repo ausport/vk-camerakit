dec2767cfac12a86bf8a5a8ec2460830adcc1f0e
Stuart Morgan <stuart.morgan@ausport.gov.au>
2022-05-10 12:59:36 +1000

    Exposure time and fps added to camera control widget.

--- widgets/camera_control_widget.py
+++ widgets/camera_control_widget.py
@@ -9,32 +9,43 @@ SUPPORTED_DEVICES = ["VKCameraGenericDevice", "VKCameraVimbaDevice"]
 
 
 class CameraControllerWidget(QWidget):
-    def __init__(self, parent, device):
+    def __init__(self, parent, devices):
 
         # The camera control widget should be initialised with a known camera device.
         # The device should be a VKCamera - either VKCameraGenericDevice, or VKCameraVimbaDevice
-        assert device.__class__.__name__ in SUPPORTED_DEVICES, "A supported VKCamera class is required."
+        for device in devices:
+            assert device.__class__.__name__ in SUPPORTED_DEVICES, "A supported VKCamera class is required."
 
         QWidget.__init__(self)
+
+        # The parent class should implement a callback <update_current_camera_device>
         self._parent = parent
-        self._device = device
-        print(self._device)
+        if not hasattr(parent, "update_current_camera_device"):
+            print("Implement <update_current_camera_device> "
+                  "in the parent instance to receive device selection callbacks.")
+            raise NotImplementedError
+
+        self._devices = devices.copy()
+        self._current_active_device = None
+        if len(self._devices) > 0:
+            self._current_active_device = self._devices[0]
 
         # Arrange layout
         camera_vb = QVBoxLayout(self)
 
-        # # Resultant image scale slider
-        # self.pano_scale_slider = QSlider(Qt.Horizontal)
-        # self.pano_scale_slider.setValue(30)
-        # self.pano_scale_slider.setMinimum(5)
-        # self.pano_scale_slider.setMaximum(100)
-        # self.pano_scale_slider.valueChanged.connect(self.refresh_parameters)
-        # self.pano_scale_label = QLabel("Output Scale: {0}%".format(self.pano_scale_slider.value()))
-        # self.pano_scale_hb = QHBoxLayout(self)
-        # self.pano_scale_hb.addWidget(self.pano_scale_slider)
-        # self.pano_scale_hb.addWidget(self.pano_scale_label)
+        # Device selection
+        self.cboDeviceSelection = QComboBox(self)
+        for device in devices:
+            self.cboDeviceSelection.addItem(device.name())
 
-        # Warping Mode
+        self.cboDeviceSelection.setCurrentText(devices[0].name())
+        self.lblDeviceSelection = QLabel("Available Devices")
+        self.cboDeviceSelection.currentIndexChanged.connect(self.refresh_device)
+        self.device_selection_hb = QHBoxLayout(self)
+        self.device_selection_hb.addWidget(self.cboDeviceSelection)
+        self.device_selection_hb.addWidget(self.lblDeviceSelection)
+
+        # Resolution Mode
         self.cboImageResolutionMode = QComboBox(self)
         self.cboImageResolutionMode.addItem("400x400")
         self.cboImageResolutionMode.addItem("320x240")
@@ -43,68 +54,90 @@ class CameraControllerWidget(QWidget):
         self.cboImageResolutionMode.addItem("1620x1080")
         self.cboImageResolutionMode.addItem("1620x1220")
         self.cboImageResolutionMode.addItem("1920x1440")
-
-        self.cboImageResolutionMode.currentIndexChanged.connect(self.refresh_widget)
         self.lblImageResolutionMode = QLabel("Image Resolution Mode")
-
         self.image_resolution_mode_hb = QHBoxLayout(self)
         self.image_resolution_mode_hb.addWidget(self.cboImageResolutionMode)
         self.image_resolution_mode_hb.addWidget(self.lblImageResolutionMode)
 
-        # # Blend Mode
-        # self.cboBlendMode = QComboBox(self)
-        # self.cboBlendMode.addItem("multiband")
-        # self.cboBlendMode.addItem("feather")
-        # self.cboBlendMode.currentIndexChanged.connect(self.refresh_widget)
-        # self.lblBlendMode = QLabel("Blend Type")
-        #
-        # self.blend_mode_hb = QHBoxLayout(self)
-        # self.blend_mode_hb.addWidget(self.cboBlendMode)
-        # self.blend_mode_hb.addWidget(self.lblBlendMode)
-        #
-        # # Blending strength
-        # self.blend_strength_slider = QSlider(Qt.Horizontal)
-        # self.blend_strength_slider.setValue(25)
-        # self.blend_strength_slider.setMinimum(0)
-        # self.blend_strength_slider.setMaximum(50)
-        # self.blend_strength_slider.valueChanged.connect(self.refresh_widget)
-        # self.blend_strength_label = QLabel("Blend Strength {0}".format(self.blend_strength_slider.value()))
-        # self.blend_strength_hb = QHBoxLayout(self)
-        # self.blend_strength_hb.addWidget(self.blend_strength_slider)
-        # self.blend_strength_hb.addWidget(self.blend_strength_label)
+        # Exposure
+        self.exposure_slider = QSlider(Qt.Horizontal)
+        self.exposure_slider.setValue(1)
+        self.exposure_slider.setMinimum(1)
+        self.exposure_slider.setMaximum(100000)
+        self.exposure_slider.valueChanged.connect(self.update_slider_controls)
+        self.exposure_label = QLabel("Exposure (μs): {0}".format(self.exposure_slider.value()))
+        self.exposure_hb = QHBoxLayout(self)
+        self.exposure_hb.addWidget(self.exposure_slider)
+        self.exposure_hb.addWidget(self.exposure_label)
+
+        # FPS
+        self.fps_slider = QSlider(Qt.Horizontal)
+        self.fps_slider.setValue(25)
+        self.fps_slider.setMinimum(1)
+        self.fps_slider.setMaximum(60)
+        self.fps_slider.valueChanged.connect(self.update_slider_controls)
+        self.fps_label = QLabel("F.P.S.: {0}".format(self.fps_slider.value()))
+        self.fps_hb = QHBoxLayout(self)
+        self.fps_hb.addWidget(self.fps_slider)
+        self.fps_hb.addWidget(self.fps_label)
 
         # Update the camera
         self.btnApplySettings = QToolButton(self)
         self.btnApplySettings.setText('Apply Settings')
         self.btnApplySettings.clicked.connect(self.apply_parameters)
 
+        # Layout widgets
+        camera_vb.addLayout(self.device_selection_hb)
         camera_vb.addLayout(self.image_resolution_mode_hb)
-        # camera_vb.addLayout(self.warp_mode_hb)
-        # camera_vb.addLayout(self.blend_mode_hb)
-        # camera_vb.addLayout(self.blend_strength_hb)
+        camera_vb.addLayout(self.exposure_hb)
+        camera_vb.addLayout(self.fps_hb)
         camera_vb.addWidget(self.btnApplySettings)
 
         self.setLayout(camera_vb)
         self.setWindowTitle("Camera Control Parameters")
 
-    def refresh_widget(self):
+        # Refresh components
+        self.refresh_widget_with_camera_properties()
+
+    def refresh_widget_with_camera_properties(self):
         # Update values and slider labels.
-        # self.pano_scale_label.setText("Output Scale: {0}%".format(self.pano_scale_slider.value()))
-        # self.blend_strength_label.setText("Blend Strength {0}".format(self.blend_strength_slider.value()))
-        pass
+        # Get resolution for current device..
+        if self._devices is not None:
+            _w = self._current_active_device.width()
+            _h = self._current_active_device.height()
+            self.cboImageResolutionMode.setCurrentText("{0}x{1}".format(_w, _h))
+
+        # Sliders
+        self.fps_label.setText("F.P.S.: {0}".format(int(self._current_active_device.fps())))
+        self.fps_slider.setValue(self._current_active_device.fps())
+        self.exposure_label.setText("Exposure (μs): {0}".format(self.exposure_slider.value()))
+        self.exposure_slider.setValue(self._current_active_device.exposure_time() * 1e3)
+
+    def update_slider_controls(self):
+        self.fps_label.setText("F.P.S.: {0}".format(int(self.fps_slider.value())))
+        self.exposure_label.setText("Exposure (μs): {0}".format(self.exposure_slider.value()))
+
+    def refresh_device(self):
+        _i = self.cboDeviceSelection.currentIndex()
+        self._current_active_device = self._devices[_i]
+        print(self._current_active_device)
+        self.refresh_widget_with_camera_properties()
 
     def apply_parameters(self):
         """Applies current capture parameters to the capture device.
         This method assumes that the parent class has a function <parent.update_capture_params(params)>
         """
 
-        if not hasattr(self._device, "set_capture_parameters"):
+        if not hasattr(self._current_active_device, "set_capture_parameters"):
             raise NotImplementedError
 
         # Parse image resolution
-        print(self.cboImageResolutionMode.currentText())
         config = {"CAP_PROP_FRAME_WIDTH": self.cboImageResolutionMode.currentText().split("x")[0],
-                  "CAP_PROP_FRAME_HEIGHT": self.cboImageResolutionMode.currentText().split("x")[1]}
+                  "CAP_PROP_FRAME_HEIGHT": self.cboImageResolutionMode.currentText().split("x")[1],
+                  "CAP_PROP_EXPOSURE": self.exposure_slider.value(),
+                  "CAP_PROP_FPS": self.fps_slider.value(),
+                  }
 
-        self._device.set_capture_parameters(configs=config)
-
+        self._current_active_device.set_capture_parameters(configs=config)
+        self.refresh_widget_with_camera_properties()
+        self._parent.update_current_camera_device(camera=self._current_active_device)
