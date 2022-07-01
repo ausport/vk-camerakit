5ed0105d56ee1d28eb86028a2f9893c584545d12
Stuart Morgan <stuart.morgan@ausport.gov.au>
2022-07-01 10:43:18 +1000

    Setup package distribution.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vk-camerakit',
+    version='0.0.1',
+    author='Stuart Morgan (Australian Institute of Sport)',
+    packages=['cameras',
+              'cameras.helpers',
+              'models',
+              'widgets']
+)
