6bcb5ed3ead7bed93fef19aa85ce62256b12912c
Stuart Morgan <stuart.morgan@ausport.gov.au>
2023-06-15 11:54:26 +1000

    Fixed a typo enumerating the device class.

--- 0.enumerate_cameras.py
+++ 0.enumerate_cameras.py
@@ -11,7 +11,7 @@ vimba_cameras = cameras.enumerate_vimba_devices()
 
 for camera in vimba_cameras:
     # Add vimba camera object to VKCamera wrapper.
-    print("Generic Camera Found:", camera_model.__class__)
+    print("Vimba-Compatible Camera Found:", camera_model.__class__)
     camera_model = cameras.VKCameraVimbaDevice(device_id=camera.get_id())
     if camera_model.is_available():
         # print(camera_model)
