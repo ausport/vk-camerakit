bf969059905da32f7814421f7f199ae2a7532a58
Stuart Morgan <stuart.morgan@ausport.gov.au>
2023-06-20 12:46:27 +1000

    Added commandline args.

--- 3.sync_camera_grab_threaded.py
+++ 3.sync_camera_grab_threaded.py
@@ -1,61 +1,106 @@
 import cameras
 import os
-import cv2
 
-# Check for Vimba cameras
-vimba_cameras = cameras.enumerate_vimba_devices()
-choice = 0
+import argparse
+import os.path
+
 CAPTURE_PATH = "./capture"
 
-if not os.path.exists(CAPTURE_PATH):
-    os.mkdir(CAPTURE_PATH)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Command-line argument parser')
+    parser.add_argument('-v', '--view', action='store_true', help='Enable viewing')
+    parser.add_argument('-f', '--flip', action='store_true', help='Flip viewing')
+    parser.add_argument('-c', '--camera_id', default=None, help='Camera ID (optional)')
+    parser.add_argument('-l', '--limit', type=int, default=None, help='Limit integer (optional)')
+    parser.add_argument('-d', '--destination', default=CAPTURE_PATH, help='Destination path (optional)')
+    return parser.parse_args()
 
-if len(vimba_cameras) > 1:
-    available_devices = []
 
-    for camera in vimba_cameras:
-        # Add vimba camera object to VKCamera wrapper.
-        camera_model = cameras.VKCameraVimbaDevice(device_id=camera.get_id())
-        print("Vimba-Compatible Camera Found:", camera.__class__)
+def expand_tilde(path):
+    return os.path.expanduser(path)
 
-        if camera_model.is_available():
-            available_devices.append(camera.get_id())
 
-    print("Select an option:")
-    for i, option in enumerate(available_devices):
-        print(f"{i}. {option}")
+def main():
+    args = parse_args()
+    camera_id = args.camera_id
+    enable_view = args.view
+    limit = args.limit
+    destination = args.destination
+    flip = args.flip
 
-    while True:
-        choice = input("Enter your choice (0-{0}): ".format(len(available_devices)-1))
+    # Interpret tilde in the destination path, if provided
+    if destination:
+        destination = expand_tilde(destination)
 
-        if choice == "":
-            choice = 0
-            break
+    # Print the parsed arguments
+    print(f'Camera ID: {camera_id}')
+    print(f'View enabled: {enable_view}')
+    print(f'Limit: {limit}')
+    print(f'Destination: {destination}')
 
-        if not choice.isdigit() or int(choice) < 0 or int(choice) > len(available_devices):
-            print("Invalid choice. Please try again.")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    # Find camera id if selected, otherwise enumerate and user-choice.
+    vimba_cameras = []
+    choice = 0
+
+    if camera_id is not None:
+        vimba_cameras.append(cameras.get_camera(camera_id))
+
+    else:
+        # Check for all Vimba cameras
+        available_device_ids = []
+        vimba_cameras = cameras.enumerate_vimba_devices()
+
+        if len(vimba_cameras) == 0:
+            print("No Vimba-compatible devices were found.")
+            exit(1)
+
         else:
-            break
+            for camera in vimba_cameras:
+                # Add vimba camera object to VKCamera wrapper.
+                camera_model = cameras.VKCameraVimbaDevice(device_id=camera.get_id())
+                print("Vimba-Compatible Camera Found:", camera.__class__)
 
-elif len(vimba_cameras) == 0:
-    exit(1)
+                if camera_model.is_available():
+                    available_device_ids.append(camera.get_id())
 
-camera = cameras.VKCameraVimbaDevice(device_id=vimba_cameras[int(choice)].get_id(), capture_path=CAPTURE_PATH)
+            print("Select an option:")
+            for i, option in enumerate(available_device_ids):
+                print(f"{i}. {option}")
 
-camera.set_image_rotation(cameras.VK_ROTATE_180)
+            while True:
+                choice = input("Enter your choice (0-{0}): ".format(len(available_device_ids)-1))
 
-_destination_width = 1456
-_destination_height = 1088
-_destination_fps = 25
-_destination_path = "/home/stuart/Desktop"
+                if choice == "":
+                    choice = 0
+                    break
 
-# NB- Vimba camera capture calls need to exist in a Vimba context.
-with camera.vimba_instance():
-    with camera.vimba_camera() as cam:
+                if not choice.isdigit() or int(choice) < 0 or int(choice) > len(available_device_ids):
+                    print("Invalid choice. Please try again.")
+                else:
+                    break
 
-        camera.generate_frames(vimba_device=cam,
-                               w=_destination_width, h=_destination_height,
-                               path=_destination_path,
-                               fps=_destination_fps, limit=1000,
-                               show_frames=True)
+    camera = cameras.VKCameraVimbaDevice(device_id=vimba_cameras[int(choice)].get_id(), capture_path=CAPTURE_PATH)
 
+    if flip:
+        camera.set_image_rotation(cameras.VK_ROTATE_180)
+
+    _destination_width = 1456
+    _destination_height = 1088
+    _destination_fps = 25
+
+    # NB- Vimba camera capture calls need to exist in a Vimba context.
+    with camera.vimba_instance():
+        with camera.vimba_camera() as cam:
+
+            camera.generate_frames(vimba_device=cam,
+                                   w=_destination_width, h=_destination_height,
+                                   path=destination,
+                                   fps=_destination_fps, limit=limit,
+                                   show_frames=True)
+
+
+if __name__ == '__main__':
+    main()
