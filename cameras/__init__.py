6003ca11c330cd80d780c56b9eb588ab44617b0f
Stuart Morgan <stuart.morgan@ausport.gov.au>
2023-06-20 12:46:16 +1000

    Exposed get-cameras.

--- cameras/__init__.py
+++ cameras/__init__.py
@@ -25,7 +25,7 @@ except ImportError:
 
 try:
     from cameras.camera_vimba import VKCameraVimbaDevice
-    from cameras.helpers.vimba_utilities import enumerate_vimba_devices
+    from cameras.helpers.vimba_utilities import enumerate_vimba_devices, get_camera
     import vmbpy as vimba
     print("#\t\t* Vimba SDK Supported:", vimba.__version__)
 except ImportError:
