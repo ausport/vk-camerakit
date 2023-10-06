2b0ec39576ace9b0286eae84de959c83a352c132
Stuart Morgan <stuart.morgan@ausport.gov.au>
2023-10-06 16:53:07 +1100

    Removed redundant file writing and viewing code.

--- cameras/helpers/vimba_utilities.py
+++ cameras/helpers/vimba_utilities.py
@@ -1,20 +1,11 @@
-import sys
-
 from vmbpy import *
 from typing import Optional
 import cv2
-import numpy as np
 import threading
-import time
-from datetime import datetime
 import queue
 
 import cameras
 from cameras import VKCamera
-
-opencv_display_format = PixelFormat.Bgr8
-
-FEATURE_MAX = -1
 
 
 def enumerate_vimba_devices():
@@ -43,6 +34,9 @@ def get_camera(camera_id: Optional[str]) -> Camera:
 
 
 def setup_pixel_format(cam: Camera):
+    # Pixel format
+    opencv_display_format = PixelFormat.Bgr8
+
     # Query available pixel formats. Prefer color formats over monochrome formats
     cam_formats = cam.get_pixel_formats()
     cam_color_formats = intersect_pixel_formats(cam_formats, COLOR_PIXEL_FORMATS)
@@ -74,6 +68,7 @@ def set_nearest_value(cam: Camera, feat_name: str, feat_value: int):
     # it calculates the nearest valid value and sets the result. This function is intended to
     # be used with Height and Width Features because not all Cameras allow the same values
     # for height and width.
+    FEATURE_MAX = -1
     with cam:
         feat = cam.get_feature_by_name(feat_name)
 
@@ -108,24 +103,11 @@ def set_nearest_value(cam: Camera, feat_name: str, feat_value: int):
             Log.get_instance().info(msg.format(cam.get_id(), feat_name, feat_value, val))
 
 
-def get_utc_timestamp_ms():
-    return int(time.time() * 1000)
-
-
 class VimbaASynchronousStreamHandler:
     def __init__(self, camera: VKCamera):
         self.shutdown_event = threading.Event()
-        self._writer = None
-        self._show_frames = False
         self._parent_camera = camera
-        self._frame_handler = VimbaASynchronousFrameHandler(parent_async_handler=self, video_writer=None)
-
-    # def set_video_writer(self, video_writer):
-    #     self._writer = video_writer
-    #     self._frame_handler = VimbaASynchronousFrameHandler(parent_async_handler=self, video_writer=self._writer)
-
-    def set_show_frames(self, show_frames):
-        self._show_frames = show_frames
+        self._frame_handler = VimbaASynchronousFrameHandler(parent_async_handler=self)
 
     def has_queued_frames(self):
         return self._frame_handler.cache_size > 0
@@ -141,53 +123,25 @@ class VimbaASynchronousStreamHandler:
         return self._frame_handler.next_frame_from_queue()
 
     def __call__(self, cam: Camera, stream: Stream, frame: Frame):
-        ENTER_KEY_CODE = 13
-
-        key = cv2.waitKey(1)
-        if key == ENTER_KEY_CODE:
-            self._frame_handler.shutdown_event.set()
-            self.shutdown_event.set()
-            return
-
         if frame.get_status() == FrameStatus.Complete:
-
-            # Since we might want to perform several operation on each frame (e.g. write, CV,...)
-            # we convert the image to opencv format here, and only once.
+            # We convert the image to opencv format here, and only once.
             converted_frame = frame.convert_pixel_format(PixelFormat.Bgr8)
             undistorted_opencv_image = self._parent_camera.undistorted_image(converted_frame.as_opencv_image())
 
             if self._parent_camera.image_rotation is not cameras.VK_ROTATE_NONE:
                 undistorted_opencv_image = cv2.rotate(undistorted_opencv_image, self._parent_camera.image_rotation)
 
-            # Get the current UTC time
-            # current_utc_time = datetime.utcnow()
-            # formatted_utc_time = current_utc_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-
             # Queue the frame with the frame handler
             self._frame_handler(frame=undistorted_opencv_image)
-            # print(f"{formatted_utc_time} The stream handler got a frame...{self._frame_handler.cache_size} frames are still queued...")
-
-            # if self._show_frames:
-            #     key = cv2.waitKey(1)
-            #     if key == ENTER_KEY_CODE:
-            #         self._frame_handler.shutdown_event.set()
-            #         self.shutdown_event.set()
-            #         return
-            #
-            #     msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'
-            #     cv2.imshow(msg.format(cam.get_id()), undistorted_opencv_image)
 
         cam.queue_frame(frame)
 
 
 class VimbaASynchronousFrameHandler:
-    def __init__(self, parent_async_handler, video_writer):
+    def __init__(self, parent_async_handler):
         self.parent = parent_async_handler
-        # self.video_writer = video_writer
         self.frame_queue = queue.Queue()
         self.shutdown_event = threading.Event()
-        # self.thread = threading.Thread(target=self._write_frames)
-        # self.thread.start()
 
     def __call__(self, frame):
         if not self.shutdown_event.is_set():
@@ -195,43 +149,12 @@ class VimbaASynchronousFrameHandler:
 
     def next_frame_from_queue(self):
         """
-        Retrieve a frame from the queue.
-
-        Returns:
-            An undistorted opencv-compatible frame in ndarray form.
+        Return a valid opencv frame from the queue.
         """
         if self.frame_queue.empty():
             return None
         else:
             return self.frame_queue.get()
-
-    # def _write_frames(self):
-    #     time.sleep(0.1)
-    #     print(self.shutdown_event.is_set())
-    #
-    #     while not self.shutdown_event.is_set(): #or not self.frame_queue.empty():
-    #
-    #         if self.frame_queue.empty():
-    #             # Wait briefly to avoid excessive CPU usage
-    #             time.sleep(1)
-    #
-    #             if self.frame_queue.empty():
-    #                 if self.video_writer:
-    #                     if self.video_writer.isOpened():
-    #                         self.video_writer.release()
-    #
-    #                 self.shutdown_event.set()
-    #
-    #         else:
-    #             # NB: The frame should already be in ndarray form.
-    #             undistorted_opencv_image = self.frame_queue.get()
-    #             print(f"Ate a frame {undistorted_opencv_image.size} - {self.cache_size} frames left.")
-    #
-    #             if self.video_writer:
-    #                 self.video_writer.write(np.asarray(undistorted_opencv_image))
-    #
-    #             time.sleep(0.1)
-
 
     @property
     def cache_size(self):
