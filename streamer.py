10cff2c6638a943f9dbf812b150ce608ec096cdc
Stuart Morgan <stuart.morgan@ausport.gov.au>
2023-10-13 16:11:48 +1100

    Multiprocessing caching frames from multiple contexts in parallel.

--- streamer.py
+++ streamer.py
@@ -1,7 +1,9 @@
-import cameras
+import multiprocessing
 import time
+
+import cameras
 import threading
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from vmbpy import *
 
 all_cameras = [
@@ -9,11 +11,8 @@ all_cameras = [
     cameras.VKCameraVimbaDevice(device_id="DEV_000F315DE932", streaming_mode=True),
 ]
 
-# Create and start threads for each camera
-camera_threads = []
 
-
-def stream(camera):
+def stream(camera, frame_queue):
     with VmbSystem.get_instance():
         print(camera.device_id ,"-->", camera.video_object.get_interface_id())
         with camera.vimba_camera() as vimba_device:
@@ -23,30 +22,51 @@ def stream(camera):
                                                    "CAP_PROP_FPS": 25,
                                                    })
 
-            streaming_thread = None
-
             # Create a non-blocking thread to run the streaming function
-            if camera.streaming_mode() == cameras.VIMBA_CAPTURE_MODE_ASYNCRONOUS:
-                streaming_thread = threading.Thread(target=camera.start_streaming,
-                                                    args=(vimba_device,
-                                                          None,
-                                                          False))
+            streaming_thread = threading.Thread(target=camera.start_streaming,
+                                                args=(vimba_device,
+                                                      None,
+                                                      False))
 
-                camera_threads.append(streaming_thread)
-                streaming_thread.start()
+            if camera.device_id == "DEV_000F315DE930":
+                # Let one of the cameras lag behind...
                 time.sleep(5)
-                print(camera.device_id, "-->", camera.cache_size)
-                camera.stop_streaming()
-                streaming_thread.join()
+
+            streaming_thread.start()
+
+            while camera.cache_size < 200:
+                if camera.cache_size % 20 == 0:
+                    print(f"{camera.device_id} has queued {camera.cache_size} frames..")
+                    time.sleep(0.5)
+
+            camera.stop_streaming()
+            streaming_thread.join()
+            print(f"{camera.device_id} finished with {camera.cache_size} frames..")
+
+            while camera.cache_size > 0:
+                frame = camera.get_frame()
+                if camera.cache_size % 20 == 0:
+                    print(f"{camera.device_id} still has {camera.cache_size} frames..")
+                frame_queue.put(frame)
+
+            print(f"{camera.device_id} has completed..")
 
 
-process1 = Process(target=stream, args=(all_cameras[0],))
-process2 = Process(target=stream, args=(all_cameras[1],))
+if __name__ == '__main__':
+    frame_queue = Queue()
 
-process1.start()
-process2.start()
+    process1 = Process(target=stream, args=(all_cameras[0], frame_queue))
+    process2 = Process(target=stream, args=(all_cameras[1], frame_queue))
 
-# Wait for processes to finish
-process1.join()
-process2.join()
+    process1.start()
+    process2.start()
+
+    while True:
+        time.sleep(10)
+        print("Killing 1")
+        process1.kill()
+        print("Killing 2")
+        process2.kill()
+        break
+
 
